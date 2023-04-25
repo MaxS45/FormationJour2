f5a15650372aacccafe228224acde10ba033a53a
msimonovici <maxime.simonovici@insee.fr>
2023-04-25 11:38:01 +0000

    fonctions isolées

---
+++ functions.R
@@ -0,0 +1,14 @@
+decennie_a_partir_annee <- function(annee) {
+  return(annee - annee %% 10)
+}
+
+fonction_de_stat_agregee <- function(a, b = "moyenne", ...) {
+  if (b == "moyenne") {
+    x <- mean(a, na.rm = TRUE, ...)
+  } else if (b == "ecart-type" || b == "sd") {
+    x <- sd(a, na.rm = TRUE, ...)
+  } else if (b == "variance") {
+    x <- var(a, na.rm = TRUE, ...)
+  }
+  return(x)
+}
