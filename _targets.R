c0a2017861b633774796d7e0ad736af1ac049832
msimonovici <maxime.simonovici@insee.fr>
2023-04-25 14:47:13 +0000

    corrections

--- _targets.R
+++ _targets.R
@@ -1,13 +1,15 @@
 library(targets)
 source("R/functions.R")
-tar_option_set(packages = c("dplyr", "ggplot2","forcats","renv"))
+tar_option_set(packages = c("dplyr", "ggplot2","forcats","renv","gt","arrow"))
 list(
   tar_target(file_data, "secrets.yaml", format = "file"), 
   tar_target(file_token, read_yaml_secret(secret)),
   tar_target(file_data, "individu_reg.parquet", format = "file"),
-  tar_target(data, read_from_parquet(file_data)),
+  tar_target(data, read_from_parquet(file_data), format="parquet"),
   tar_target(clean_data, retraitement_donnees(data)),
   tar_target(table_age, produce_table_age(clean_data))
-
 )
+tar_make()
+
+
 
