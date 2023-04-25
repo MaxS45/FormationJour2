d762d87b44dc49e9a8f6098f6100b7687a2c0281
msimonovici <maxime.simonovici@insee.fr>
2023-04-25 14:43:36 +0000

    définition de target

--- _targets.R
+++ _targets.R
@@ -2,9 +2,12 @@ library(targets)
 source("R/functions.R")
 tar_option_set(packages = c("dplyr", "ggplot2","forcats","renv"))
 list(
-  tar_target(file, "data.csv", format = "file"),
-  tar_target(data, get_data(file)),
-  tar_target(model, fit_model(data)),
-  tar_target(plot, plot_model(model, data))
+  tar_target(file_data, "secrets.yaml", format = "file"), 
+  tar_target(file_token, read_yaml_secret(secret)),
+  tar_target(file_data, "individu_reg.parquet", format = "file"),
+  tar_target(data, read_from_parquet(file_data)),
+  tar_target(clean_data, retraitement_donnees(data)),
+  tar_target(table_age, produce_table_age(clean_data))
+
 )
 
